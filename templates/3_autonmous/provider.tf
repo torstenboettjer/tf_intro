3098d315332fd0c3cf0ae929a45984c8796e7202
torstenboettjer <torsten.boettjer@oracle.com>
2021-11-24 19:56:18 +0000

    tenancy_ocid

--- templates/3_autonmous/provider.tf
+++ templates/3_autonmous/provider.tf
@@ -2,6 +2,7 @@
 // terraform apply -var tenancy_ocid=$OCI_TENANCY -auto-approve
 
 provider "oci" {}
+variable "tenancy_ocid" {}
 
 data "oci_core_images" "service" {
     compartment_id = var.compartment_id
